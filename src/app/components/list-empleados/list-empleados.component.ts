d1d320818e5c4e5e6b6886a173f98a68efe92cc5
SteveenHuawei <ss.rodriguez00021@gmail.com>
2022-06-07 17:49:26 -0500

    obtenemos todos los datos de firebase en list-empl

--- src/app/components/list-empleados/list-empleados.component.ts
+++ src/app/components/list-empleados/list-empleados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 // import { AngularFirestore } from '@angular/fire/compat/firestore';
 // import { Observable } from 'rxjs';
 
@@ -13,11 +14,38 @@ export class ListEmpleadosComponent implements OnInit {
 
   // firestore: AngularFirestore (injectado en el constructor)
   // this.items = firestore.collection('items').valueChanges() --> (dentro de llaves constructor)
-  constructor() {
+
+  empleados: any[] = [];
+
+  // Inyectamos el servicio EmpleadoService
+  constructor(private empleadoService :  EmpleadoService) {
 
    }
 
   ngOnInit(): void {
+    this.getEmpleados();
+  }
+
+  // este método lo utilizaremos cuando se inicialice el componente por tanto lo llamamos en el ngOnInit()
+  getEmpleados(){
+    // al utilizar el servicio es un observable por tanto nos suscribimos
+    this.empleadoService.getEmpleados()
+      .subscribe(data => {
+        console.log(data)
+        data.forEach((element:any) => {
+          // element.payload.doc.id --> obtiene el id de los elementos en firebase
+          //element.payload.doc.data() --> obtiene los datos completos de firebase
+          // console.log(element.payload.doc.data())
+
+          // se guarda en empleados[]
+          this.empleados.push({
+            id: element.payload.doc.id, //le agregamos el id en una variable nueva
+            ...element.payload.doc.data(), //con sprint-operator concatemos el id con data
+          })
+
+
+        });
+      });
   }
 
 }

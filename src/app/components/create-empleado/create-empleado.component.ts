24d749cbda3aa1f90cbeef7f836fb56ceb8e7952
SteveenHuawei <ss.rodriguez00021@gmail.com>
2022-06-07 16:42:42 -0500

    se conecta el objeto del formulario con firebase

--- src/app/components/create-empleado/create-empleado.component.ts
+++ src/app/components/create-empleado/create-empleado.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-create-empleado',
@@ -12,7 +13,9 @@ export class CreateEmpleadoComponent implements OnInit {
   subbmited = false;
 
   //inyectamos la clase FormsBuilder para construir form reactivos
-  constructor(private fb: FormBuilder) {
+  // Inyectamos el servicio EmpleadoService
+  constructor(private fb: FormBuilder,
+              private empleadoService: EmpleadoService) {
 
     this.createEmpleado = this.fb.group({
       nombre: ['', Validators.required],
@@ -43,8 +46,12 @@ export class CreateEmpleadoComponent implements OnInit {
       fechaActualizacion: new Date()
 
     }
+    // se llama el servicio y se le pasa el objeto empleado
+    // es una promesa y por tanto se utiliza el then con un arrow Función
+    this.empleadoService.agregarEmpleado(empleado)
+      .then(() => console.log('Empleado Registrado '))
+      .catch(error => console.log(error));
 
-    console.log(empleado)
   }
 
 }
--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadoService {
 
-  constructor() { }
+  // Inyección de AngularFirestore
+  constructor(private firestore: AngularFirestore) { }
+
+  agregarEmpleado(empleado : any) : Promise<any> {
+
+    // se llama la inyección de angularFiresStore
+    // se crea una collection con el nombre en string
+    // se agrega el objeto que recibe el método
+    return this.firestore.collection('empleados').add(empleado)
+  }
+
 }

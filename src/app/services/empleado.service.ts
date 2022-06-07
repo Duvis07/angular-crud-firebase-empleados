31bb461f05d893edcebba3dc10c96d61c41bc3a4
SteveenHuawei <ss.rodriguez00021@gmail.com>
2022-06-07 17:30:42 -0500

    se agrega el servico getEmpleados, get all data

--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,20 @@ export class EmpleadoService {
   // Inyección de AngularFirestore
   constructor(private firestore: AngularFirestore) { }
 
+
+  // Servicio que agrega un empleado
   agregarEmpleado(empleado : any) : Promise<any> {
 
     // se llama la inyección de angularFiresStore
     // se crea una collection con el nombre en string
     // se agrega el objeto que recibe el método
     return this.firestore.collection('empleados').add(empleado)
+  }
+
+  // Servicio que obtiene todos los datos de firebase
+  getEmpleados(): Observable<any> {
+    // llamamos a la inyección firestore y al método snapshotChanges el cual Cree una secuencia de cambios sincronizados. Este método mantiene la matriz local en el orden de consulta ordenado. devuelve un observable
+    return this.firestore.collection('empleados').snapshotChanges();
   }
 
 }
